2dbd6b39337ce6eb57445f5e9fca3594adf88bb5
O MY BRAND <39580047+bajama@users.noreply.github.com>
2020-03-24 15:49:14 +0900

    complete

--- js/todo.js
+++ js/todo.js
@@ -1,45 +1,69 @@
 
 const toDoForm = document.querySelector(".toDoForm"),
-    toDoinput = toDoForm.querySelector("input")
+    toDoInput = toDoForm.querySelector("input")
+const toDoList = document.querySelector(".toDoList")
 
 let toDos = []
 
-function handleToDo(event){
-
-}
-
-function createToDo(toDo){
+function createToDo(value){
     const li = document.createElement("li")
-    const delBtn = document.createElement("span")
-    const label = document.createElement("label")
-    const ul = document.querySelector(".toDoList")
-    label.innerText = toDo
-    label.appendChild(li)
-    delBtn.appendChild(li)
-    li.appendChild(ul)
+    li.className = "toDo"
+    li.id = toDos.length + 1;
+    const deletBtn = document.createElement("button")
+    deletBtn.innerText = "❌"
+    deletBtn.className = "toDo_button"
+    deletBtn.addEventListener("click",deleteToDo)
+    const span = document.createElement("span")
+    span.innerText = value
+    li.appendChild(deletBtn)
+    li.appendChild(span)
+    toDoList.appendChild(li)
+    updateToDo(value)
 }
 
-function readToDo(load){
+function readToDo(){
     const currentToDo = localStorage.getItem("toDos")
     if(currentToDo !== null){
-    const parseToDo = JSON.parse(currentToDo)
-        parseToDo.forEach(function(element){
-            console.log(element)
-        })
+        const parsedToDo = JSON.parse(currentToDo)
+        parsedToDo.forEach(function(toDo) {
+            createToDo(toDo.text)
+        });
+      }
     }
 
+function localSet(){
+    const stringToDo = JSON.stringify(toDos)
+localStorage.setItem("toDos",stringToDo)
 }
 
-function updateToDo(save){
-
+function updateToDo(value){
+const toDoObj = {
+    id : toDos.length + 1,
+    text : value
+}
+toDos.push(toDoObj)
+localSet()
 }
 
-function deleteToDo(){
-
+function deleteToDo(event){
+    const li = event.target.parentElement
+    li.remove(li.id)
+    toDos =toDos.filter(function(toDo){
+        return toDo.id !== parseInt(li.id)
+    })
+    localSet()
 }
 
+function onsummit(event){
+    event.preventDefault();
+    const value = toDoInput.value
+    createToDo(value)
+    toDoInput.value = ""
+}
 
 function init(){
- readToDo()
+    readToDo()
+    toDoForm.addEventListener("submit",onsummit)
 }
+
 init()
